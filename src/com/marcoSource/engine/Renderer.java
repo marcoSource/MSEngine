734c10d7b3f3f36b597fa7c2dec9c531004f85f7
marcoSource <gammma7869@gmail.com>
2018-09-14 15:17:19 +0100

    Redone the image drawing method
    Now the image is rendered when is visible

--- src/com/marcoSource/engine/Renderer.java
+++ src/com/marcoSource/engine/Renderer.java
@@ -21,8 +21,8 @@ public class Renderer {
         }
     }
 
-    public void setPixels(int x, int y, int value){
-        if((x < 0 || x >= pixelWidth || y < 0 || y >= pixelHeight) && value == 0xffff00ff){
+    public void setPixels(int x, int y, int value) {
+        if ((x < 0 || x >= pixelWidth || y < 0 || y >= pixelHeight) && value == 0xffff00ff) {
             return;
         }
 
@@ -30,8 +30,28 @@ public class Renderer {
     }
 
     public void drawImage(Image image, int offX, int offY) {
-        for (int y = 0; y < image.getHeight(); y++){
-            for (int x = 0; x < image.getWidth(); x++){
+        //Dont render
+        if(offX < -image.getWidth()) { return; }
+        if(offY < -image.getHeight()) { return; }
+        if(offX >= pixelWidth) {return;}
+        if(offY >= pixelHeight){ return;}
+
+        int newX = 0;
+        int newY = 0;
+        int newWidth = image.getWidth();
+        int newHeight = image.getHeight();
+
+
+
+
+        //Render what is visible
+        if(offX < 0) { newX -= offX; }
+        if(offY < 0) { newY -= offY; }
+        if(newWidth + offX >= pixelWidth) { newWidth -= newWidth + offX - pixelWidth; }
+        if(newHeight + offY >= pixelHeight) { newHeight -= newHeight + offY - pixelHeight; }
+
+        for (int y = 0; y < newHeight; y++) {
+            for (int x = 0; x < newWidth; x++) {
                 setPixels(x + offX, y + offY, image.getPixels()[x + y * image.getWidth()]);
             }
         }
