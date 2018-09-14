a5fc7edd3195dce1160d9aac7dad3ba7be673365
marcoSource <gammma7869@gmail.com>
2018-09-14 15:41:18 +0100

    drawTile method in renderer

--- src/com/marcoSource/engine/Renderer.java
+++ src/com/marcoSource/engine/Renderer.java
@@ -1,6 +1,7 @@
 package com.marcoSource.engine;
 
 import com.marcoSource.engine.gfx.Image;
+import com.marcoSource.engine.gfx2d.ImageTile;
 
 import java.awt.image.DataBufferInt;
 
@@ -41,9 +42,6 @@ public class Renderer {
         int newWidth = image.getWidth();
         int newHeight = image.getHeight();
 
-
-
-
         //Render what is visible
         if(offX < 0) { newX -= offX; }
         if(offY < 0) { newY -= offY; }
@@ -53,6 +51,31 @@ public class Renderer {
         for (int y = 0; y < newHeight; y++) {
             for (int x = 0; x < newWidth; x++) {
                 setPixels(x + offX, y + offY, image.getPixels()[x + y * image.getWidth()]);
+            }
+        }
+    }
+
+    public void drawImageTile(ImageTile image, int offX, int offY, int tileX, int tileY){
+        //Dont render
+        if(offX < -image.getTileWidth()) { return; }
+        if(offY < -image.getTileHeight()) { return; }
+        if(offX >= pixelWidth) {return;}
+        if(offY >= pixelHeight){ return;}
+
+        int newX = 0;
+        int newY = 0;
+        int newWidth = image.getTileWidth();
+        int newHeight = image.getTileHeight();
+
+        //Render what is visible
+        if(offX < 0) { newX -= offX; }
+        if(offY < 0) { newY -= offY; }
+        if(newWidth + offX >= pixelWidth) { newWidth -= newWidth + offX - pixelWidth; }
+        if(newHeight + offY >= pixelHeight) { newHeight -= newHeight + offY - pixelHeight; }
+
+        for (int y = 0; y < newHeight; y++) {
+            for (int x = 0; x < newWidth; x++) {
+                setPixels(x + offX, y + offY, image.getPixels()[(x + tileX * image.getTileWidth()) + (y + tileY * image.getTileHeight()) * image.getWidth()]);
             }
         }
     }
